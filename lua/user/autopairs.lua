cf59b786a824c08e2047af8ee615384b1c314505
ccbiozhaw <patc@zhaw.ch>
2022-09-30 13:48:17 +0200

    fast_wrap changes

--- lua/user/autopairs.lua
+++ lua/user/autopairs.lua
@@ -7,6 +7,16 @@ end
 npairs.setup {
   check_ts = true, -- treesitter integration
   disable_filetype = { "TelescopePrompt" },
+  fast_wrap = {
+    map = '<M-e>',
+    chars = { '{', '[', '(', '"', "'" },
+    pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey='Comment'
+    },
 }
 
 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
--- test.py
+++
@@ -1,2 +0,0 @@
-def somefn():
-    print("hello")
