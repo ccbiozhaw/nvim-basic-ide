7c4452861a574e16ebef04c9e6e8dca6d1ebd392
ccbiozhaw <patc@zhaw.ch>
2022-10-01 21:54:22 +0200

    added winbar plugin and removed buffers

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -50,6 +50,7 @@ return packer.startup(function(use)
   use { "kyazdani42/nvim-tree.lua" }
   use { "akinsho/bufferline.nvim" }
   use { "moll/vim-bbye" }
+  use { 'fgheng/winbar.nvim' }
   use { "nvim-lualine/lualine.nvim" }
   use { "akinsho/toggleterm.nvim" }
   use { "ahmedkhalf/project.nvim" }
