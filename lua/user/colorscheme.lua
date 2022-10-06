ac11566dfa84382b17dca8024cde7e98a321568d
ccbiozhaw <patc@zhaw.ch>
2022-10-06 16:49:36 +0200

    colors

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,9 +1,12 @@
 local colorscheme = "catppuccin"
-
 vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+
+--[[ vim.cmd [[ ]]
+--[[ colorscheme tokyonight ]]
+--[[ ]] 
 
