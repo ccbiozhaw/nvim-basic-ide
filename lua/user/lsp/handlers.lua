b73d10b143f773566c919217d75189ad39545f8c
ccbiozhaw <patc@zhaw.ch>
2022-10-13 09:22:54 +0200

    no tsserver format

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -83,7 +83,7 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.server_capabilities.document_formatting = true
+    client.server_capabilities.document_formatting = false
   end
 
   if client.name == "sumneko_lua" then
